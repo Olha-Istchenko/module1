4fb448d7c2b4afc206c05fac04b660d1bf4a94a2
Olha <Olha.Ishchenko@simcorp.com>
2024-11-04 10:18:55 +0100

    first commit

---
+++ HelloFromC#/Program.cs
@@ -0,0 +1,23 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello everybody");
+
+//Console.WriteLine("Please enter your firstName: ");
+//string firstName = Console.ReadLine();
+
+//Console.WriteLine("Please enter your lastName: ");
+//string lastName = Console.ReadLine();
+
+//Console.WriteLine("Hello " + firstName + "" + lastName);
+
+Console.WriteLine("Enter a value: ");
+int max = int.Parse(Console.ReadLine());
+int i = 0;
+
+while (i < max)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+Console.WriteLine("Loop finished!");
+
